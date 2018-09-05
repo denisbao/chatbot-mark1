036cd310b799ec0fa34e5fc72701034263e4bee0
Denis Bao Motta <denis.tpd@gmail.com>
2018-09-05 16:54:01 -0300

    testes

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -18,11 +18,10 @@ class BotController extends Controller
     {
         $webhook = new WebHook();
         $subscribe = $webhook->check(config('botfb.validationToken'));
-        dd($subscribe);
-        var_dump($subscribe);
-//        if (!$subscribe){
-//            abort(403, "Unauthorized action.");
-//        }
+
+        if (!$subscribe){
+            abort(403, "Unauthorized action.");
+        }
         return $subscribe;
     }
 

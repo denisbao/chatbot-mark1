2e35dfc7de97ac2581eb188f052ebc1068744ae6
Denis Bao Motta <denis.tpd@gmail.com>
2018-09-10 16:07:02 -0300

    revisando código

--- config/botfb.php
+++ config/botfb.php
@@ -1,7 +1,6 @@
 <?php
 
-
 return [
     'pageAccessToken' =>  env('FB_PAGE_ACCESS_TOKEN'),
-    'validationToken' => env ('FB_VALIDATION_TOKEN'),
+    'validationToken' => env('FB_VALIDATION_TOKEN'),
 ];

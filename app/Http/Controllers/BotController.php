dda0edc22504260b83f655763b320ad230bd2608
Denis Bao Motta <denis.tpd@gmail.com>
2018-09-13 09:51:00 -0300

    teste resposta de imagem

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -7,7 +7,11 @@ namespace App\Http\Controllers;
  */
 
 use CodeBot\CallSendApi;
+use CodeBot\Message\Audio;
+use CodeBot\Message\File;
+use CodeBot\Message\Image;
 use CodeBot\Message\Text;
+use CodeBot\Message\Video;
 use CodeBot\SenderRequest;
 use CodeBot\WebHook;
 use Illuminate\Http\Request;
@@ -34,8 +38,26 @@ class BotController extends Controller
         $text = new Text($senderId);
         $callSendApi = new CallSendApi(config('botfb.pageAccessToken'));
 
+        //TESTE PARA ENVIO DE TEXTO:
         $callSendApi->make($text->message('Olá, eu sou um bot...'));
         $callSendApi->make($text->message('Você digitou: '. $message));
+
+//        //TESTE PARA ENVIO DE AUDIO:
+//        $message = new Audio($senderId);
+//        $callSendApi->make($text->message($message->message('https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/256x256/robot.png')));
+//
+//        //TESTE PARA ENVIO DE ARQUIVOS:
+//        $message = new File($senderId);
+//        $callSendApi->make($text->message($message->message('https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/256x256/robot.png')));
+//
+//        //TESTE PARA ENVIO DE VIDEOS:
+//        $message = new Video($senderId);
+//        $callSendApi->make($text->message($message->message('https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/256x256/robot.png')));
+
+        //TESTE PARA ENVIO DE IMAGENS:
+        $message = new Image($senderId);
+        $callSendApi->make($text->message($message->message('https://boiling-dawn-10043.herokuapp.com/img/robot.png')));
+
 
     }
 }

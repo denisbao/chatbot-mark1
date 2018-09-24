45b5f0664798a18fee28028147ad5d9baed50abd
Denis Bao Motta <denis.tpd@gmail.com>
2018-09-24 15:51:12 -0300

    removendo alguns tipos de mensagem

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -44,29 +44,29 @@ class BotController extends Controller
         $callSendApi->make($text->message('Olá, eu sou um bot...'));
         $callSendApi->make($text->message('Você digitou: '. $message));
 
-        //TESTE PARA ENVIO DE AUDIO:
-        $message = new Audio($senderId);
-        $callSendApi->make($text->message('Escuta uma musiquinha ai:'));
-        $callSendApi->make($message->message('https://boiling-dawn-10043.herokuapp.com/audio/audio-test.mp3'));
-
-        //TESTE PARA ENVIO DE ARQUIVOS:
-        $message = new File($senderId);
-        $callSendApi->make($text->message('Confira esse arquivo ai:'));
-        $callSendApi->make($message->message('https://boiling-dawn-10043.herokuapp.com/file/arquivo.zip'));
-
-        //TESTE PARA ENVIO DE VIDEOS:
-        $message = new Video($senderId);
-        $callSendApi->make($text->message('Já viu esse video?'));
-        $callSendApi->make($message->message('https://boiling-dawn-10043.herokuapp.com/video/video-test.mp4'));
-
-        //TESTE PARA ENVIO DE IMAGENS:
-        $message = new Image($senderId);
-        $callSendApi->make($text->message('Quer ver um nude?'));
-        $callSendApi->make($message->message('https://boiling-dawn-10043.herokuapp.com/img/robot.png'));
+//        //TESTE PARA ENVIO DE AUDIO:
+//        $message = new Audio($senderId);
+//        $callSendApi->make($text->message('Escuta uma musiquinha ai:'));
+//        $callSendApi->make($message->message('https://boiling-dawn-10043.herokuapp.com/audio/audio-test.mp3'));
+//
+//        //TESTE PARA ENVIO DE ARQUIVOS:
+//        $message = new File($senderId);
+//        $callSendApi->make($text->message('Confira esse arquivo ai:'));
+//        $callSendApi->make($message->message('https://boiling-dawn-10043.herokuapp.com/file/arquivo.zip'));
+//
+//        //TESTE PARA ENVIO DE VIDEOS:
+//        $message = new Video($senderId);
+//        $callSendApi->make($text->message('Já viu esse video?'));
+//        $callSendApi->make($message->message('https://boiling-dawn-10043.herokuapp.com/video/video-test.mp4'));
+//
+//        //TESTE PARA ENVIO DE IMAGENS:
+//        $message = new Image($senderId);
+//        $callSendApi->make($text->message('Quer ver um nude?'));
+//        $callSendApi->make($message->message('https://boiling-dawn-10043.herokuapp.com/img/robot.png'));
 
         $message = new ButtonsTemplate($senderId);
-        $message->add(new Button('web_url', '(9Gag', 'https://www.9gag.com'));
-        $message->add(new Button('web_url', '(Google', 'https://www.google.com'));
+        $message->add(new Button('web_url', '9Gag', 'https://www.9gag.com'));
+        $message->add(new Button('web_url', 'Google', 'https://www.google.com'));
         $callSendApi->make($message->message('Testando botões'));
 
     }
